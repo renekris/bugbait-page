11a7333e25e81a9d82f0f466221d23727fb012b3
Renekris <renekrispohlak@gmail.com>
2022-09-30 06:13:26 +0300

    Add content and footer functions

--- src/menu.js
+++ src/menu.js
@@ -3,9 +3,9 @@ function drawMenu() {
     const elContainer = document.getElementById('container');
 
     // MAIN
-    createHeader();
-
     elContainer.append(createHeader());
+    elContainer.append(createContent());
+    elContainer.append(createFooter());
 }
 
 function createHeader() {
@@ -45,7 +45,46 @@ function createHeader() {
 function createContent() {
     // CREATE PARENT
     const elWrapper = document.createElement('div');
-    const elContent = document.createElement('content');
+    const elContent = document.createElement('div');
+
+    // CONTENT
+    elWrapper.className = 'content-wrapper';
+    elContent.id = 'content';
+
+    const elH1 = document.createElement('h1');
+    elH1.textContent = 'Half-Life Bugbait';
+    elContent.appendChild(elH1);
+
+
+    let elP = document.createElement('p');
+    elP.textContent = 'Half-Life Bugbait is a store where Half-Life items are available to purchase.';
+    elContent.appendChild(elP);
+
+    elP = document.createElement('p');
+    elP.textContent = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam officiis, quaerat ab provident esse asperiores ut aliquam facilis! Doloremque fugit enim facere culpa ad nam, cumque repellat voluptatibus eius nostrum! Debitis commodi fuga dicta aut temporibus, quia accusamus molestiae eos possimus at rem. Quisquam assumenda odit ut expedita impedit vitae asperiores hic, iure deserunt quasi doloremque consequuntur tenetur consectetur! Perspiciatis. Inventore id, commodi magnam, laborum, in tempora dolores tenetur cupiditate vitae alias nobis impedit.';
+    elContent.appendChild(elP);
+
+    elP = document.createElement('p');
+    elP.textContent = 'Fuga in voluptates eligendi enim quis facilis deleniti adipisci, architecto saepe omnis quisquam nobis sed harum? Ullam exercitationem sunt tempore reiciendis cumque iusto voluptatum odio saepe quae repellat, natus error eos velit vel officiis aliquid cum a quasi ut! Veritatis, aut. Minus excepturi odio nemo animi! Aliquid consequatur blanditiis totam sed aspernatur deserunt assumenda consequuntur quam, earum ducimus sint et.';
+    elContent.appendChild(elP);
+
+
+
+    elWrapper.appendChild(elContent);
+    // RETURN
+    console.log(elWrapper);
+    return elWrapper;
+}
+
+function createFooter() {
+    const elFooter = document.createElement('div');
+    elFooter.id = 'footer';
+
+    const elP = document.createElement('p');
+    elP.textContent = '© 2022 Renekris. All bugs baited.';
+    elFooter.appendChild(elP);
+
+    return elFooter;
 }
 
 export { drawMenu };
